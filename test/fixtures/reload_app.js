131f4da1e9cdae81caa5fc8b41e88f0104c53113
Michael Welter <me@mikinho.com>
2026-01-25 08:25:00 -0600

    Speed up the reload test
    Signed-off-by: Michael Welter <me@mikinho.com>

--- test/fixtures/reload_app.js
+++ test/fixtures/reload_app.js
@@ -3,14 +3,22 @@ import http from "node:http";
 import cluster from "node:cluster";
 
 const control = run(() => {
-    http.createServer((req, res) => res.end("ok")).listen(0);
+    http.createServer((req, res) => res.end("ok"))
+        .listen(0)
+        .on("error", (err) => {
+            // Ignore EPIPE/ECONNRESET during reload/shutdown sequences
+            if (err.code !== "EPIPE" && err.code !== "ECONNRESET") {
+                console.error("Server error:", err);
+            }
+        });
     // console.log(`Worker ${process.pid} started`);
     // Send a message to master to log PID? 
     // Or just log from worker. Master pipes stdout.
     console.log(`PID:${process.pid}`);
 }, {
     minWorkers: 2,
-    mode: "smart"
+    mode: "smart",
+    shutdownTimeout: 1000
 });
 
 // Master logic to trigger reload
